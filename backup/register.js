548e310ac4cf58d3892dc59c3fd95e1dc0682f5c
Aspect-6 <neelpatel246624@gmail.com>
2023-03-26 18:41:39 -0400

    node

--- backup/register.js
+++ backup/register.js
@@ -1,8 +1,5 @@
-import * as firebase from './firebaseConfig.js';
 
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-
-const crypto = require('crypto');
+//const crypto = require('crypto');
 
 document.getElementById('login-box').style.transform = 'scale(1)';
 document.getElementById('login-box').style.transition = '0s';
@@ -101,9 +98,3 @@ document.getElementById('password').addEventListener('focus', function() {
         document.getElementById('usernameLabel').style.transform = 'translateY(-50%)';
     }
 });
-
-
-
-
-
-},{}]},{},[1]);
